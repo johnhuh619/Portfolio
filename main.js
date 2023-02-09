e83066e01e23f7044c1cf88c3509b687fdd86b44
johnhuh619 <jaewonheo666@gmail.com>
2023-02-09 18:56:32 +0900

    JS_Scroll to target

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Make navbar transport whe it tis on the top
-const navbar = document.querySelecwhetor('#navbar');
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   console.log(window.scrollY);
@@ -11,4 +11,18 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
